ee497210dbe402dfd6946cdd7a6c57d21412372c
AshVoller <thine.technocrat@gmail.com>
2018-09-22 00:34:34 -0700

    Adding first pages

---
+++ TS_Ticket_Brought_In_Creation.js
@@ -0,0 +1,12 @@
+var Client_ID
+var Tech_First_Name
+var Tech_Last_Name
+var Type_Of_Tech
+
+var Customer_First_Name
+var Customer_Last_Name
+var Customer_Phone
+var Customer_Email
+
+var PID
+var Box _Type
