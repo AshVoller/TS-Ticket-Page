d7bbc4fd2892d535b423ba0b3054a7cb278d9b81
AshVoller <thine.technocrat@gmail.com>
2018-10-01 12:37:44 -0700

    Html clean up. JS for pulling labels to JSON.

--- TS_Ticket_Brought_In_Creation.js
+++ TS_Ticket_Brought_In_Creation.js
@@ -1,3 +1,30 @@
+const formInfo = {};
+
+window.addEventListener('load', function() {
+  const form = document.getElementById('Form_TS_Ticket_Brought_In_Creation');
+
+  form.addEventListener('submit', function (e) {
+    console.log(e);
+    e.preventDefault();
+    const keys = Object.keys(e.currentTarget);
+
+    for(let i=0; i< keys.length; i++) {
+      const key = keys[i];
+      const element = e.currentTarget[key];
+      if (element.type !== 'submit') {
+        const label = element.labels[0].childNodes[0].textContent;
+        const value = element.value;
+
+        formInfo[label] = value;
+      }
+
+    }
+
+    console.log(formInfo)
+  })
+})
+
+/*
 var Client_ID
 var Tech_First_Name
 var Tech_Last_Name
@@ -19,3 +46,4 @@ var Summary_Of_Issue
 var Detailed_Description_Of_Issue
 var Backup_Data
 var Quoted_Price
+*/
