a6d64f61328d1573b1a41d5a4d72210f4299cd36
AshVoller <thine.technocrat@gmail.com>
2018-10-11 16:29:11 -0700

    Fixed Second Page Filling Need CSS touches in page Need to Test pdfmake Need to touch up form values.

--- TS_Ticket_Page.js
+++ TS_Ticket_Page.js
@@ -9,12 +9,15 @@ window.addEventListener('load', function() {
   const Form_Data = JSON.parse(JSON_Data);
   console.log(Form_Data);
   for(let i=0;i<Object.keys(Form_Data).length;i++) {
-    const Form_Key = Object.keys(Form_Data)[i];
-    console.log(Form_Key);
-    console.log(Object.values(Form_Data[Form_Key]));
+    let Form_Label = Object.keys(Form_Data)[i];
+    console.log(Form_Label);
+    let Form_Value = Form_Data[Form_Label];
+    console.log(Form_Value);
+    //console.log(Form_Data[Form_Label]);
+    //console.log(Object.values(Form_Key).toString());
     //const Form_Value = Form_Data[Form_Key].value;
     //console.log(Form_Value);
-    //document.getElementById(Form_Key).innerHTML = Form_Value;
+    document.getElementById(Form_Label).innerHTML = Form_Value;
   }
 
 })
